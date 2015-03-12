d2b5f0ce753aba2baf6d5b8c3b6225b8f1cfd1f3
stormx5 <mn6@live.com>
2015-03-11 18:49:57 -0700

    First commit

--- src/main/java/valifation_Project/validation/DropBoxAPI.java
+++ src/main/java/valifation_Project/validation/DropBoxAPI.java
@@ -10,6 +10,7 @@ public class DropBoxAPI {
 	final String app_secret = "zfgn6o6ial7552o";
 	
 	public boolean CheckDropboxAPI() throws IOException, DbxException{
+		// Get API info by supplying the AppKey and AppSecret
 		DbxAppInfo appinfo = new DbxAppInfo(app_key, app_secret);
 		
 		DbxRequestConfig config = new DbxRequestConfig("CS585Test", Locale.getDefault().toString());
@@ -21,9 +22,11 @@ public class DropBoxAPI {
         String accessToken = authFinish.accessToken;
 
         DbxClient client = new DbxClient(config, accessToken);
-
+        //This line test if the API is working for Account info retrieval 
         System.out.println("Linked account: " + client.getAccountInfo().displayName);
         
+        // The test is done here for another Service which gives the remaining size on that account. 
+        //However, I am not showing the size remaining, I am only checking if I get a numnber or not, so If I got
         long dropboxsize = -1;
         DbxAccountInfo acctinfo = client.getAccountInfo();
         dropboxsize = acctinfo.quota.total / 1024 / 1024 / 1024;
